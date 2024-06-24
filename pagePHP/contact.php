6da48b03e02790d1950924dda6c54fbe8ae96d35
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2024-06-24 16:47:30 +0200

    etape 5 formulaire - verification des donée recu + indiquer a l'user l'erreure

--- pagePHP/contact.php
+++ pagePHP/contact.php
@@ -1,14 +1,103 @@
 <?php include('header.php'); ?>
 <main>
-    <section id="Section1">
+    <section id="CONTACT">
         <h2>Page de contact</h2>
-        <div class=div1>
-            <img src="img/drole-et-agreable-homme-charnu-montrant-welcome-sign-vector-illustration-ettn1x.jpg" alt="">
-            <div class="div2">    
-                <h3>BONGIORNO</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis bibendum tortor, eu vulputate nibh convallis eget. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In ligula leo, bibendum in feugiat hendrerit, vulputate sed lacus. Ut viverra magna eu interdum euismod. Integer nec ipsum mi. Proin fermentum, justo eu dictum tempus, erat sapien auctor eros, ut tempus enim tortor ac dolor. Cras consequat volutpat tempor. Integer vel ex feugiat, rhoncus mauris nec, tincidunt sem.Mauris velit ligula, vestibulum vel vestibulum quis, malesuada eget ligula. Vestibulum imperdiet dui sit amet arcu aliquam dignissim. Pellentesque fringilla iaculis ex, nec fringilla augue accumsan quis. Pellentesque posuere ultrices nisl non pretium. Cras laoreet suscipit nulla, in sollicitudin nisl. Nulla eleifend non ipsum sed vestibulum. Phasellus faucibus, lectus ut volutpat euismod, nibh quam imperdiet turpis, et sagittis risus ligula ac metus. Sed consequat feugiat quam efficitur feugiat. Vestibulum lacinia sem pellentesque, placerat lacus at, volutpat enim. Suspendisse placerat, risus eget placerat placerat, ipsum massa fringilla risus, eu hendrerit risus sem vitae odio.</p>
-            </div>
+
         <div>
+            <form class="formulaireContact" action="index.php?page=contact" method="POST">
+                <label for="civilite">Civilité:</label>
+                <select name="civilite" id="civilite">
+                    <option value="Monsieur">Monsieur</option>
+                    <option value="Madame">Madame</option>
+                    <option value="Mademoiselle">airbus A242</option>
+                </select>
+
+                <label for="nom">Nom:</label>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if(empty($_POST['nom'])){
+                        echo "<p class='wrong'>Entré un nom valide</p>";
+                    }
+                }
+                ?>
+                <input type="text" id="nom" name="nom">
+
+                <label for="prenom">Prénom:</label>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if(empty($_POST['prenom'])){
+                        echo "<p class='wrong'>Entré un prenom valide</p>";
+                    }
+                }
+                ?>
+                <input type="text" id="prenom" name="prenom">
+
+                <label for="email">Email:</label>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                        echo "<p class='wrong'>Entré un email valide</p>";
+                    }
+                }
+                ?>
+                <input type="email" id="email" name="email">
+
+                <label>Raison du contact:</label>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if(empty($_POST['raison'])){
+                        if($_POST['raison']!='Service comptable' || $_POST['raison']!='Support technique' || $_POST['raison']!='Autre'){
+                            echo "<p class='wrong'>Selectionner l'un des 3 choix</p>";
+                        }
+                    }
+                }
+                ?>
+                <input type="radio" id="service_comptable" name="raison" value="Service comptable">
+                <label for="service_comptable">Service comptable</label>
+                <input type="radio" id="support_technique" name="raison" value="Support technique">
+                <label for="support_technique">Support technique</label>
+                <input type="radio" id="autre" name="raison" value="Autre">
+                <label for="autre">Autre</label>
+
+                <label for="message">Message:</label>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if ( strlen($_POST['message'])<5){
+                        echo '<p class="wrong">message trop court</p>';
+                    }
+                }
+                ?>
+                <textarea id="message" name="message" rows="4" cols="50"></textarea>
+
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if( strlen($_POST['message'])>=5 && !empty($_POST['nom']) && !empty($_POST['prenom']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                if($_POST['raison']=='Service comptable' || $_POST['raison']=='Support technique' || $_POST['raison']=='Autre'){
+                    $civilite = $_POST['civilite'];
+                    $nom = ($_POST['nom']);
+                    $prenom = ($_POST['prenom']);
+                    $email = htmlspecialchars($_POST['email']);
+                    $raison = $_POST['raison'];
+                    $message = htmlspecialchars($_POST['message']);
+
+                    echo "
+                    <div class='reponseFormContact'>
+                    <h4>Merci pour votre message, $civilite $nom</h4>
+                    <p>Prénom: $prenom</p>
+                    <p>Email: $email</p>
+                    <p>Raison du contact: $raison</p>
+                    <p>Message: $message</p>
+                    </div>
+                    ";
+                }
+            }
+        }
+        ?>
     </section>
 </main>
+
 <?php include('footer.php'); ?>
